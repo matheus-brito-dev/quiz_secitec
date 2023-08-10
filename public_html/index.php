27519470adc2db586b1ec0ac3910c7a0e3bd4f49
Matheus Brito <matheus@MBP-de-Matheus.bbrouter>
2023-08-10 18:41:26 -0300

    first_comit_quiz_secitec

---
+++ public_html/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Página principal do sistema
+ * tudo começa aqui
+ * 
+ */
---
+++ public_html/modules/category/pages/createCategories.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Todo módulo tem que ter essa estrutrua
+ * com uma camada de visão, uma de controller e uma do model
+ * 
+ * O model intermedia os dados antes de irem pro banco e quando 
+ * saem do banco e voltam pra aplicação
+ * 
+ * o controller está no meio recebendo a requisição da view e 
+ * chamando o método correto para alterar o banco e devolver a 
+ * resposta pra view
+ * 
+ * A view é somente a página em html puro ou mesclado ou usando 
+ * javascript
+ */
